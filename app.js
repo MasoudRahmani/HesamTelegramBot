f5737bf4c3b393491c0a193d6ddf0d4371a5cfac
Masoud Rahmani <mrem71@gmail.com>
2021-12-03 11:27:26 +0330

    heroku port

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 'use strict';
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 /* -----------------  TEXT ----------------*/
 class person {
     constructor(id, username, name, country, number, ques) {
